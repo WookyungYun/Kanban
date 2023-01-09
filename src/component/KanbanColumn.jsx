b2b51a634a1f7e129ccf9d0f7fbbf6d1c4f1c3a9
WookyungYun <gyong1540@gmail.com>
2023-01-09 17:57:24 +0900

    feat: 타이틀 수정

--- src/component/KanbanColumn.jsx
+++ src/component/KanbanColumn.jsx
@@ -3,13 +3,30 @@ import AddItemBtn from "./AddItemBtn";
 import KanbanItem from "./KanbanItem";
 import ContentEditable from "react-contenteditable";
 import DeleteColumnBtn from "./DeleteColumnBtn";
+import { useRecoilState } from "recoil";
+import { columnsState } from "../store/kanban";
+import { deepCopy } from "../utils/object";
 
 export default function KanbanColumn({ column, index }) {
+  const [columns, setColumns] = useRecoilState(columnsState);
+  const handleChange = (e) => {
+    const clone = deepCopy(columns);
+    if (e.target.value !== "") {
+      clone[index].title = e.target.value;
+      setColumns(clone);
+    } else {
+      alert("제목을 입력해주세요.");
+    }
+  };
   return (
     <>
       <div>
         <strong>
-          <ContentEditable html={column.title} disabled={false} />
+          <ContentEditable
+            html={column.title}
+            disabled={false}
+            onChange={handleChange}
+          />
           <DeleteColumnBtn parentColumn={column} index={index} />
         </strong>
         <Droppable droppableId={`${index}`} index={index}>
